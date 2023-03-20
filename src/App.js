7993b510a7c94707ed9e0a002331afcda1ea9a17
Milgam06 <milgamfruit@gmail.com>
2023-03-20 18:32:20 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	new file:   .eslintrc
    	new file:   .prettierrc
    	new file:   .vscode/settings.json
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React, { useState, useRef } from 'react';
-
-
+import React, { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
   function PlusClicking() {
-    setCount(count + 1)
+    setCount(count + 1);
   }
   function MinusClicking() {
-    setCount(count - 1)
+    setCount(count - 1);
   }
   function ResetClicking() {
-    setCount(0)
+    setCount(0);
   }
   return (
     <div>
       <h3>Total Click: {count}</h3>
-      <button id='plusBtn' onClick={PlusClicking}>+</button>
-      <button id='minusBtn' onClick={MinusClicking}>-</button>
-      <br/>
-      <button id="resetBtn" onClick={ResetClicking}>RESET</button>
+      <button id="plusBtn" onClick={PlusClicking}>
+        +
+      </button>
+      <button id="minusBtn" onClick={MinusClicking}>
+        -
+      </button>
+      <br />
+      <button id="resetBtn" onClick={ResetClicking}>
+        RESET
+      </button>
     </div>
   );
 }
