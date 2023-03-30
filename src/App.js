1f9c1c7766ba930e9e5827e61d703c0a24c260b5
Milgam06 <milgamfruit@gmail.com>
2023-03-30 19:28:41 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [toDo, setToDo] = useState('');
-  const [toDos, setToDos] = useState([]);
-  const Changing = (event) => {
-    setToDo(event.target.value);
-  };
-
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === '') {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo('');
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [value, setValue] = useState('');
   useEffect(() => {
-    console.log(toDos);
-  }, [toDos]);
+    fetch('https://api.coinpaprika.com/v1/tickers')
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  function onChange(event) {
+    setValue(event.target.value);
+  }
+  function onReset() {
+    setValue('');
+  }
   return (
     <div>
-      <h1>My toDo {toDos.length}</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={Changing}
-          value={toDo}
-          type="text"
-          placeholder="Write you to do...."
-        ></input>
-        <button>Add ToDo..</button>
-      </form>
+      <h1>The coin! {loading ? '' : coins.length}</h1>
+      <input onChange={onChange} value={value} placeholder="bedgit" type={'number'}></input>
+      <button onClick={onReset}>Reset</button>
+      <br></br>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option>
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price}$
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
