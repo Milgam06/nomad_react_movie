2029779d4b15009a4945de603650240dc02c07ad
Milgam06 <milgamfruit@gmail.com>
2023-03-31 13:24:30 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -2,41 +2,23 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [value, setValue] = useState('');
-  useEffect(() => {
-    fetch('https://api.coinpaprika.com/v1/tickers')
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-  function onChange(event) {
-    setValue(event.target.value);
-  }
-  function onReset() {
-    setValue('');
-  }
-  return (
-    <div>
-      <h1>The coin! {loading ? '' : coins.length}</h1>
-      <input onChange={onChange} value={value} placeholder="bedgit" type={'number'}></input>
-      <button onClick={onReset}>Reset</button>
-      <br></br>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option>
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price}$
-            </option>
-          ))}
-        </select>
-      )}
-    </div>
-  );
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const respone = await fetch(
+      'ttps://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year',
+    );
+    const json = await respone.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect =
+    (() => {
+      getMovies();
+    },
+    []);
+
+  console.log(movies);
+  return <div>{loading ? <h1>Loading...</h1> : movies.map}</div>;
 }
 
 export default App;
