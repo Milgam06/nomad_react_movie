0eded741a3fb408656b7d80cb0c73c1a118fa12c
Milgam06 <milgamfruit@gmail.com>
2023-04-01 11:02:14 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
+  const KEY = '455b10de026d03f0f6c3f5ebded6fb60';
+  const DATE = '20220101';
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async () => {
-    const respone = await fetch(
-      'ttps://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year',
-    );
-    const json = await respone.json();
-    setMovies(json.data.movies);
+    const json = await (
+      await fetch(
+        `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${KEY}&targetDt=${DATE}`,
+      )
+    ).json();
+    setMovies(json.boxOfficeResult.dailyBoxOfficeList);
     setLoading(false);
   };
-  useEffect =
-    (() => {
-      getMovies();
-    },
-    []);
+  useEffect(() => {
+    getMovies();
+  }, []);
 
-  console.log(movies);
-  return <div>{loading ? <h1>Loading...</h1> : movies.map}</div>;
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
