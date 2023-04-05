166cf6e02bcb9ffd294559e6faa8a106462987df
Milgam06 <milgamfruit@gmail.com>
2023-04-05 18:30:03 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/routes/Detail.js

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Detail() {
-  const x = useParams();
-  console.log(x);
-
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [gmovie, setGmovie] = useState([]);
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json);
+    setGmovie(json.data.movie);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <div>
+      <h1>{gmovie.title}</h1>
+      <img src={gmovie.medium_cover_image} alt="img"></img>
+    </div>
+  );
 }
 
 export default Detail;
