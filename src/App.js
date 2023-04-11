279155f3dea5b0258903c2ed7bd734f28fc240cf
Milgam06 <milgamfruit@gmail.com>
2023-04-11 23:27:25 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   package.json
    	modified:   src/App.js
    	new file:   src/components/Navigation.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './routes/Home';
 import Detail from './routes/Detail';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <Router>
+      <Navigation />
       <Routes>
         <Route path="/hello" element={<h1>hello</h1>} />
       </Routes>
@@ -13,7 +15,7 @@ function App() {
         <Route path="/movie/:id" element={<Detail />} />
       </Routes>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} e />
       </Routes>
     </Router>
   );
